407a29bae2984ee935edc7838a8a1085955ba325
Michał Kornafel <kornel.mk@gmail.com>
2026-06-21 20:51:36 +0200

    ci: zmiana portu aplikacji na http/80

--- infra/security-groups.tf
+++ infra/security-groups.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "app_sg" {
 
   # Reguła dla ruchu HTTP
   ingress {
-    from_port   = 8501
-    to_port     = 8501
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
