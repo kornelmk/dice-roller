b506e04673bf9afb6164af06e7cf64ac57adc994
Michał Kornafel <kornel.mk@gmail.com>
2026-06-21 19:04:02 +0200

    ci: porządki w security groups

--- infra/security-groups.tf
+++ infra/security-groups.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "frontend_sg" {
+resource "aws_security_group" "app_sg" {
   vpc_id = aws_vpc.main.id
 
   # Reguła dla ruchu HTTP
@@ -24,54 +24,5 @@ resource "aws_security_group" "frontend_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = merge(local.tags, { Name = "frontend-sg" })
-}
-
-resource "aws_security_group" "backend_sg" {
-  vpc_id = aws_vpc.main.id
-
-  # Reguła dla ruchu HTTP
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Reguła dla ruchu SSH
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(local.tags, { Name = "backend-sg" })
-}
-
-resource "aws_security_group" "db_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(local.tags, { Name = "db-sg" })
+  tags = merge(local.tags, { Name = "app-sg" })
 }
