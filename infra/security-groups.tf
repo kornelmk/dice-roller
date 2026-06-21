077c33d30154976e2b088c83a4dc3bfe9a6441bc
Michał Kornafel <kornel.mk@gmail.com>
2026-06-21 18:00:42 +0200

    ci: zmiana reguł security-groups - dodanie ruchu ssh

--- infra/security-groups.tf
+++ infra/security-groups.tf
@@ -1,9 +1,18 @@
 resource "aws_security_group" "frontend_sg" {
   vpc_id = aws_vpc.main.id
 
+  # Reguła dla ruchu HTTP
   ingress {
     from_port   = 8501
     to_port     = 8501
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Reguła dla ruchu SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -21,9 +30,18 @@ resource "aws_security_group" "frontend_sg" {
 resource "aws_security_group" "backend_sg" {
   vpc_id = aws_vpc.main.id
 
+  # Reguła dla ruchu HTTP
   ingress {
     from_port   = 8000
     to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Reguła dla ruchu SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
