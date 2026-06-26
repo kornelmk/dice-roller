18fb544571295e8d8ad23f47228d4c03f7cbd3ff
Michał Kornafel <kornel.mk@gmail.com>
2026-06-26 13:26:26 +0200

    fix: dodanie brakującego vpc_zone_identifier

--- infra/autoscaling_group.tf
+++ infra/autoscaling_group.tf
@@ -5,7 +5,10 @@ resource "aws_autoscaling_group" "app" {
   min_size         = 2
   max_size         = 4
 
-  vpc_zone_identifier = aws_subnet.public[*].id
+  vpc_zone_identifier = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
 
   target_group_arns = [aws_lb_target_group.app.arn]
 
